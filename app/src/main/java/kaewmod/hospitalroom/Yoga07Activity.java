037fc16ddcb31a4c13d40f47267a1a5807516009
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-08-18 16:43:12 +0700

    Yoga115

--- app/src/main/java/kaewmod/hospitalroom/Yoga07Activity.java
+++ app/src/main/java/kaewmod/hospitalroom/Yoga07Activity.java
@@ -3,11 +3,11 @@ package kaewmod.hospitalroom;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NoOkActivity extends AppCompatActivity {
+public class Yoga07Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_no_ok);
-}
+        setContentView(R.layout.activity_yoga07);
+    }
 }
