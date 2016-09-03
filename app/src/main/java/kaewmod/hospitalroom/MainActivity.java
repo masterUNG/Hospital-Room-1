ad62030ccba88e260de9f2b6ca03de68dafe02a3
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-09-03 15:24:53 +0700

    Radiobutton

--- app/src/main/java/kaewmod/hospitalroom/MainActivity.java
+++ app/src/main/java/kaewmod/hospitalroom/MainActivity.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, password1EditText, password2EditText;
-    private String nameString, password1String, password2String;
+    private String nameString, password1String, password2String, mainString, sexString;
+    private RadioGroup mainRadioGroup, sexRadioGroup;
+    private RadioButton main1RadioButton, main2RadioButton,
+            maleRadioButton, femaleRadioButton;
 
 
 
@@ -23,6 +28,50 @@ public class MainActivity extends AppCompatActivity {
         nameEditText = (EditText) findViewById(R.id.editText);
         password1EditText = (EditText) findViewById(R.id.editText2);
         password2EditText = (EditText) findViewById(R.id.editText3);
+        mainRadioGroup = (RadioGroup) findViewById(R.id.ragmain);
+        sexRadioGroup = (RadioGroup) findViewById(R.id.regSex);
+        main1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        main2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        maleRadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        femaleRadioButton = (RadioButton) findViewById(R.id.radioButton4);
+
+        //Create Main Radio Controler
+        mainRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.radioButton:
+                        mainString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        mainString = "1";
+                        break;
+
+                }
+            }
+        });
+        //Create Sex Radio Controller
+        sexRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.radioButton3:
+                        sexString = "male";
+                        break;
+                    case R.id.radioButton4:
+                        sexString = "female";
+                        break;
+                }
+            }
+        });
+
+
+
+
+
+
+
+
 
 
 
