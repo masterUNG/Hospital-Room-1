8d199189e4cab0ede6327e28c3206ade8bfd409a
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-09-03 14:58:59 +0700

    MyAleet

--- app/src/main/java/kaewmod/hospitalroom/MainActivity.java
+++ app/src/main/java/kaewmod/hospitalroom/MainActivity.java
@@ -4,20 +4,52 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, password1EditText, password2EditText;
+    private String nameString, password1String, password2String;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        password1EditText = (EditText) findViewById(R.id.editText2);
+        password2EditText = (EditText) findViewById(R.id.editText3);
+
+
+
+    } //Main Method
+
+    public void clickSaveData(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        password1String = password1EditText.getText().toString().trim();
+        password2String = password2EditText.getText().toString().trim();
+
+
+        //Check Space
+        if (checkSpace()) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่อง ค่ะ");
+        }
+
+    } //ClickSavedata
+
+    private boolean checkSpace() {
+        return nameString.equals("")||
+                password1String.equals("")||
+                password2String.equals("");
+
     }
 
-    public void clickLogin(View view) {
-        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
 
-    }
-
-
-
-}
+} //Main Class
---
+++ app/src/main/java/kaewmod/hospitalroom/MyAlert.java
@@ -0,0 +1,31 @@
+package kaewmod.hospitalroom;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by IT on 3/9/2559.
+ */
+public class MyAlert {
+
+ public  void myDialog(Context context,
+                       String strTitle,
+                       String strMessage){
+     AlertDialog.Builder builder = new AlertDialog.Builder(context);
+     builder.setCancelable(false);
+     builder.setIcon(R.drawable.nobita48);
+     builder.setTitle(strTitle);
+     builder.setMessage(strMessage);
+     builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+         @Override
+         public void onClick(DialogInterface dialogInterface, int i) {
+             dialogInterface.dismiss();
+         }
+     });
+     builder.show();
+
+
+ }
+} //Main Class
+
