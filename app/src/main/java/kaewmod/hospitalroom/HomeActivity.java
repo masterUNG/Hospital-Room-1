0af3f56d6bb0013a05274affcab70eaa901960f8
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-09-09 12:03:56 +0700

    finishHome

--- app/src/main/java/kaewmod/hospitalroom/HomeActivity.java
+++ app/src/main/java/kaewmod/hospitalroom/HomeActivity.java
@@ -1,14 +1,25 @@
 package kaewmod.hospitalroom;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends LoginActivity {
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-    }
-}
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent splashIntent = new Intent(HomeActivity.this, LoginActivity.class);
+                HomeActivity.this.startActivity(splashIntent);
+                HomeActivity.this.finish();
+            }
+    }, SPLASH_TIME_OUT);
+}}
+
