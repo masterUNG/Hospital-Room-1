6f721217952fcd19d7c57fc00ccaf19aaac9de1d
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-10-20 16:26:05 +0700

    20/10/59

--- app/src/main/java/kaewmod/hospitalroom/HomeActivity.java
+++ app/src/main/java/kaewmod/hospitalroom/HomeActivity.java
@@ -5,23 +5,81 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HomeActivity extends LoginActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+
+public class HomeActivity extends Activity {
+
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    /**
+     * Called when the activity is first created.
+     */
+    Thread splashTread;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        new Handler().postDelayed(new Runnable() {
+        ImageView img = (ImageView) findViewById(R.id.splash);
+        img.setBackgroundResource(R.drawable.logo_run);
+        AnimationDrawable frameAnimation = (AnimationDrawable) img.getBackground();
+        frameAnimation.start();
+
+        StartAnimations();
+    }
+
+    private void StartAnimations() {
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        anim.reset();
+
+        LinearLayout l = (LinearLayout) findViewById(R.id.lin_lay);
+        l.clearAnimation();
+        l.startAnimation(anim);
+
+        anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+
+        ImageView iv = (ImageView) findViewById(R.id.splash);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+
+        splashTread = new Thread() {
             @Override
             public void run() {
-                Intent splashIntent = new Intent(HomeActivity.this, LoginActivity.class);
-                HomeActivity.this.startActivity(splashIntent);
-                HomeActivity.this.finish();
+                try {
+                    int waited = 0;
+                    // Splash screen pause time
+                    while (waited < 5000) {
+                        sleep(100);
+                        waited += 100;
+                    }
+                    Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    HomeActivity.this.finish();
+                } catch (InterruptedException e) {
+                    // do nothing
+                } finally {
+                    HomeActivity.this.finish();
+                }
             }
-
-        }, SPLASH_TIME_OUT);
-    }}
-
-
+        };
+        splashTread.start();
+    }
+}
