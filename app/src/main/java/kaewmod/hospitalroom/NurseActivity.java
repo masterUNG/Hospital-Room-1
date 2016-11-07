c4fc477c358523a6493fbabfb95bf8c2c8bbd37f
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-11-07 11:32:11 +0700

    7/11/59

--- app/src/main/java/kaewmod/hospitalroom/NurseActivity.java
+++ app/src/main/java/kaewmod/hospitalroom/NurseActivity.java
@@ -1,9 +1,11 @@
 package kaewmod.hospitalroom;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class NurseActivity extends AppCompatActivity {
 
@@ -11,6 +13,7 @@ public class NurseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nurse);
+
     }
     public void clickNurseHead (View view) {
         startActivity(new Intent(NurseActivity.this,DiseaseheaderActivity.class));
