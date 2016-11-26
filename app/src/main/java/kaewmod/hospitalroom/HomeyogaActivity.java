55a90e8e098892e62c9bf6018519067a15340699
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-11-26 15:28:03 +0700

    last

--- app/src/main/java/kaewmod/hospitalroom/HomeyogaActivity.java
+++ app/src/main/java/kaewmod/hospitalroom/HomeyogaActivity.java
@@ -12,83 +12,84 @@ public class HomeyogaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeyoga);
     }
-    public void clickHomeyoga1 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga01Activity.class));
+
+    public void clickHomeyoga1(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga01Activity.class));
 
     }
 
-    public void clickHomeyoga2 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga02Activity.class));
+    public void clickHomeyoga2(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga02Activity.class));
 
     }
 
-    public void clickHomeyoga3 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga03Activity.class));
+    public void clickHomeyoga3(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga03Activity.class));
 
     }
 
-    public void clickHomeyoga4 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga04Activity.class));
+    public void clickHomeyoga4(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga04Activity.class));
 
     }
 
-    public void clickHomeyoga5 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga05Activity.class));
+    public void clickHomeyoga5(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga05Activity.class));
 
     }
 
-    public void clickHomeyoga6 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga06Activity.class));
+    public void clickHomeyoga6(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga06Activity.class));
 
     }
 
-    public void clickHomeyoga7 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga07Activity.class));
+    public void clickHomeyoga7(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga07Activity.class));
 
     }
 
-    public void clickHomeyoga8 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga08Activity.class));
+    public void clickHomeyoga8(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga08Activity.class));
 
     }
 
-    public void clickHomeyoga9 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga09Activity.class));
+    public void clickHomeyoga9(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga09Activity.class));
 
     }
 
-    public void clickHomeyoga10 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga10Activity.class));
+    public void clickHomeyoga10(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga10Activity.class));
 
     }
 
-    public void clickHomeyoga11 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga11Activity.class));
+    public void clickHomeyoga11(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga11Activity.class));
 
     }
 
-    public void clickHomeyoga12 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga12Activity.class));
+    public void clickHomeyoga12(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga12Activity.class));
 
     }
 
-    public void clickHomeyoga13 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga13Activity.class));
+    public void clickHomeyoga13(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga13Activity.class));
 
     }
 
-    public void clickHomeyoga14 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga14Activity.class));
+    public void clickHomeyoga14(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga14Activity.class));
 
     }
 
-    public void clickHomeyoga15 (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,Yoga15Activity.class));
+    public void clickHomeyoga15(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, Yoga15Activity.class));
 
     }
 
-    public void clickHomeyogaHome (View view) {
-        startActivity(new Intent(HomeyogaActivity.this,MenuActivity.class));
+    public void clickHomeyogaHome(View view) {
+        startActivity(new Intent(HomeyogaActivity.this, MenuActivity.class));
 
     }
 
