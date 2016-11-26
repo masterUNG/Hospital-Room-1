61cefa6cbc1f0e1eeb2f8e4fe9a16e81604368f5
lookkaew <lookkaew_aod_sbac@hotmail.com>
2016-11-26 11:58:00 +0700

    OK

--- app/src/main/java/kaewmod/hospitalroom/AddSon.java
+++ app/src/main/java/kaewmod/hospitalroom/AddSon.java
@@ -1,10 +1,14 @@
 package kaewmod.hospitalroom;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,7 +24,7 @@ import org.json.JSONObject;
 public class AddSon extends AppCompatActivity {
 
     private ListView listView;
-    private String loginString;
+    private String[] loginStrings;
 
 
 
@@ -30,6 +34,8 @@ public class AddSon extends AppCompatActivity {
         setContentView(R.layout.activity_add_son);
 
         listView = (ListView) findViewById(R.id.livSon);
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
 
 
         //Create ListView
@@ -37,6 +43,66 @@ public class AddSon extends AppCompatActivity {
         listAllSon.execute();
 
     } //Main Method
+
+    private class EditSon extends AsyncTask<Void, Void, String>{
+
+
+        private Context context;
+
+        private String idSonString;
+
+
+        public EditSon(Context context, String idSonString) {
+            this.context = context;
+            this.idSonString = idSonString;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            if () {
+            } else {
+            }
+
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("id", loginStrings[0])
+                        .add("id_son", idSonString)
+                        .build();
+                Request.Builder builder = new  Request.Builder();
+                Request request = builder.url("http://www.swiftcodingthai.com/mod/edit_son.php")
+                        .post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+
+
+
+
+            } catch (Exception e) {
+                Log.d("26novV3", "e doIn ==> " + e.toString());
+                return null;
+            }
+
+
+
+
+
+        }
+
+
+
+
+    } //EditSon
 
     private class ListAllSon extends AsyncTask<Void, Void, String>{
 
@@ -85,8 +151,8 @@ public class AddSon extends AppCompatActivity {
             try {
 
                 JSONArray jsonArray = new JSONArray(s);
-                String[] userString = new String[jsonArray.length()];
-                String[] idSonStrings = new String[jsonArray.length()];
+                final String[] userString = new String[jsonArray.length()];
+                final String[] idSonStrings = new String[jsonArray.length()];
 
                 for (int i=0;i<jsonArray.length();i++){
 
@@ -103,9 +169,46 @@ public class AddSon extends AppCompatActivity {
                         android.R.layout.simple_list_item_1, userString);
                 listView.setAdapter(stringArrayAdapter);
 
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        comfirmSon(idSonStrings[i], userString[i]);
+
+                    }
+                });
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+
+        }
+
+        private void comfirmSon(final String idSonString, String userString) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setCancelable(false);
+            builder.setIcon(R.drawable.tokjai);
+            builder.setTitle(userString);
+            builder.setMessage("You Choose" + userString);
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+
+                }
+            });
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    EditSon editSon = new EditSon(context, idSonString);
+                    editSon.execute();
+
+                    dialogInterface.dismiss();
+
+                }
+            });
+            builder.show();
 
 
         }
