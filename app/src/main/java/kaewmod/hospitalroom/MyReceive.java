ee57249ba920324c7c865c3990de89b44b930e60
masterUNG <phrombutr@gmail.com>
2016-12-22 15:55:08 +0700

    การบ้าน

--- app/src/main/java/kaewmod/hospitalroom/MyReceive.java
+++ app/src/main/java/kaewmod/hospitalroom/MyReceive.java
@@ -8,7 +8,7 @@ import android.content.Intent;
  * Created by IT on 9/12/2559.
  */
 
-public class MyReceive extends BroadcastReceiver{
+public class MyReceive extends BroadcastReceiver {
 
 
     @Override
@@ -17,6 +17,14 @@ public class MyReceive extends BroadcastReceiver{
         Intent intent1 = new Intent(context, ShowNotification.class);
 
         intent1.putExtra("Mediciene", intent.getStringExtra("Mediciene"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
+        intent1.putExtra("MonthYear", intent.getStringExtra("MonthYear"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
+        intent1.putExtra("Start", intent.getStringExtra("Start"));
 
 
         intent1.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -24,4 +32,16 @@ public class MyReceive extends BroadcastReceiver{
 
 
     } //Main Method
+
+    //    intent.putExtra("Mediciene", nameMediciene);  /// ใส่ที่เหลือ หน้าแสดงแจ้งเตือนการกินยา
+//    intent.putExtra("Login", loginStrings);
+//    intent.putExtra("Start", day );
+//    intent.putExtra("MonthYear", ("/" + month + "/" + year));
+//    intent.putExtra("TimeUse", timeUser);
+//    intent.putExtra("Morning", morning);
+//    intent.putExtra("Lunch", lunch);
+//    intent.putExtra("Diner", diner);
+//    intent.putExtra("Sleep", sleep);
+//    intent.putExtra("Food", food);
+
 } //Main Class
