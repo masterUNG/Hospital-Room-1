b5873eb55b64f8514639b54b33002d8a17b57030
masterUNG <phrombutr@gmail.com>
2017-01-13 14:13:56 +0700

    Request Id mother and son ok

--- app/src/main/java/kaewmod/hospitalroom/SonService.java
+++ app/src/main/java/kaewmod/hospitalroom/SonService.java
@@ -6,12 +6,16 @@ import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class SonService extends AppCompatActivity {
 
     //Explicit
     private String[] loginStrings;
     private TextView textView;
     private ListView listView;
+    private String idMotherString;// นี่คือ id ของแม่
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +30,7 @@ public class SonService extends AppCompatActivity {
 
         loginStrings = getIntent().getStringArrayExtra("Login");
 
-        Log.d("9decV1", "idLogin ==> " + loginStrings[0]);
+        Log.d("9decV1", "idLogin ==> " + loginStrings[0]);  // หมายถึง id ของลูก
 
 
         try {
@@ -35,6 +39,12 @@ public class SonService extends AppCompatActivity {
             synMother.execute();
             String s = synMother.get();
             Log.d("9decV1", "JSON ==> " + s);
+
+            JSONArray jsonArray = new JSONArray(s);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            idMotherString = jsonObject.getString("id");
+            Log.d("13janV3", "id ของลูก ==> " + loginStrings[0]);
+            Log.d("13janV3", "id ของแม่ ==> " + idMotherString);
 
         } catch (Exception e) {
             e.printStackTrace();
